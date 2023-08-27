f29c2911bd1b9736e59b6af55cb496dbb911577d
AlishiaNaqvi <96846213+AlishiaNaqvi@users.noreply.github.com>
2023-08-27 19:39:15 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB26fjloOjmdD8A0Hr424um_pZk4-9fRwI",
+  authDomain: "blogging-app-74c01.firebaseapp.com",
+  projectId: "blogging-app-74c01",
+  storageBucket: "blogging-app-74c01.appspot.com",
+  messagingSenderId: "14289293011",
+  appId: "1:14289293011:web:90837adda4b32751824e26"
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
